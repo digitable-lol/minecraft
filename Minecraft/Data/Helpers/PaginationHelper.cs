fa5f37be051a1dfadf8c859b53756f90119ce617
hekitociha <chernov.200125@gmail.com>
2022-06-29 00:55:37 +0300

    fix QR

---
+++ Minecraft/Data/Helpers/PaginationHelper.cs
@@ -0,0 +1,32 @@
+﻿using Minecraft.Data.Filters;
+using Minecraft.Data.Interfaces;
+using Minecraft.Data.Models;
+using Minecraft.Data.Wrappers;
+using System;
+using System.Collections.Generic;
+
+namespace Minecraft.Data.Helpers
+{
+    public class PaginationHelper
+    {
+        public static PagedResponse<IEnumerable<thing>> CreatePagedReponse<thing>(List<thing> pagedData,PaginationFilter filter, int totalRecords, IUriServiсe uriService, string route)
+        {
+            var respose = new PagedResponse<IEnumerable<thing>>(pagedData, filter.PageNumber, filter.PageSize);
+            var totalPages = ((double)totalRecords / (double)filter.PageSize);
+            int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
+            respose.NextPage =
+                filter.PageNumber >= 1 && filter.PageNumber < roundedTotalPages
+                ? uriService.GetPageUri(new PaginationFilter(filter.PageNumber + 1, filter.PageSize), route)
+                : null;
+            respose.PreviousPage =
+                filter.PageNumber - 1 >= 1 && filter.PageNumber <= roundedTotalPages
+                ? uriService.GetPageUri(new PaginationFilter(filter.PageNumber - 1, filter.PageSize), route)
+                : null;
+            respose.FirstPage = uriService.GetPageUri(new PaginationFilter(1, filter.PageSize), route);
+            respose.LastPage = uriService.GetPageUri(new PaginationFilter(roundedTotalPages, filter.PageSize), route);
+            respose.TotalPages = roundedTotalPages;
+            respose.TotalRecords = totalRecords;
+            return respose;
+        }
+    }
+}
---
+++ Minecraft/Data/Models/user.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Minecraft.Data.Models
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public List<thing> things { get; set; } 
+    }
+}
